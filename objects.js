45337587ea10f43d4fc518df6ff72af02bd47779
Anna Conaway <annaconaway19@gmail.com>
2018-07-15 17:27:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,9 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {['artistName']: 'songTitle'})
+  return Object.assign({}, playlist, {[artistName]: songTitle})
   }
+updatePlaylist(playlist, 'artistName', 'songTitle');
 
 function removeFromPlaylist(playlist, artistName) {
  delete playlist.artistName;
