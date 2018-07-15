1f173d3c7fccdb40c227a493e8ea7275ce791ede
Anna Conaway <annaconaway19@gmail.com>
2018-07-15 18:07:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,5 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   }
 
 function removeFromPlaylist() {
- return delete playlist.artistName;
+ delete playlist.artistName;
+ return playlist;
  }
